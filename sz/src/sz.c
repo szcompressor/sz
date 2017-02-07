4e370a6116d01bfa440eeae5dd1d061f5ed35a08
Sheng Di <disheng222@gmail.com>
2017-02-07 11:56:50 -0600

    allow SZ_Finalize() to be called multiple times.

--- sz/src/sz.c
+++ sz/src/sz.c
@@ -12,7 +12,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <math.h>
 #include "sz.h"
 #include "CompressElement.h"
 #include "DynamicByteArray.h"
@@ -45,8 +44,7 @@ double absErrBound;
 double relBoundRatio;
 double pw_relBoundRatio;
 int segment_size;
-
-int versionNumber[3];
+int versionNumber[3] = {SZ_VER_MAJOR,SZ_VER_MINOR,SZ_VER_REVISION};
 
 int spaceFillingCurveTransform; //default is 0, or 1 set by sz.config
 int reOrgSize; //the granularity of the reganization of the original data
@@ -116,7 +114,7 @@ int SZ_Init_Params(sz_params *params)
     	stateNum = maxRangeRadius*2;
 		allNodes = maxRangeRadius*4;
 		intvCapacity = maxRangeRadius*2;
-		intvRadius = maxRangeRadius;		
+		intvRadius = maxRangeRadius;
     }
     dataEndianType    = endianType;
     sysEndianType    = endianType;
@@ -176,9 +174,9 @@ int SZ_Init_Params(sz_params *params)
 			predThreshold = params->predThreshold;
     }
 
-	versionNumber[0] = SZ_VER_MAJOR; //0
-	versionNumber[1] = SZ_VER_MINOR; //5
-	versionNumber[2] = SZ_VER_REVISION; //15
+//	versionNumber[0] = SZ_VER_MAJOR; //0
+//	versionNumber[1] = SZ_VER_MINOR; //5
+//	versionNumber[2] = SZ_VER_REVISION; //15
 
 	if(params->quantization_intervals%2!=0)
 	{
@@ -885,19 +883,21 @@ int getPredictionCoefficients(int layers, int dimension, int **coeff_array)
 
 void SZ_Finalize()
 {
-	int i ;
-	free(pool);
-	pool = NULL;
-	free(qqq);
-	qqq = NULL;
-	for(i=0;i<stateNum;i++)
+	if(pool!=NULL)
 	{
-		if(code[i]!=NULL)
-			free(code[i]);
-	}	
-	free(code);
-	code = NULL;
-	free(cout);
-	cout = NULL;
-	
+		int i;
+		free(pool);
+		pool = NULL;
+		free(qqq);
+		qqq = NULL;
+		for(i=0;i<stateNum;i++)
+		{
+			if(code[i]!=NULL)
+				free(code[i]);
+		}
+		free(code);
+		code = NULL;
+		free(cout);
+		cout = NULL;				
+	}
 }
