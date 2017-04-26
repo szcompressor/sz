637b5425698ac0e524de50b362bedbf5a5111e4e
Sheng Di <disheng222@gmail.com>
2017-04-26 12:02:28 -0500

    revise testfloat_CompDecomp.sh

--- example/testfloat_CompDecomp.sh
+++ example/testfloat_CompDecomp.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [[ $# < 1 ]]
+then
+	echo Usage: $0 [error bound]
+	echo Example: $0 1E-4
+	exit
+fi
+
 dataDir="/home/fti/SZ_C_version/CESM-testdata/1800x3600"
-absErrBound=1E-4
+absErrBound=$1
 
 targetDir="$dataDir"
 fileList=`cd "$targetDir";ls *.dat`
