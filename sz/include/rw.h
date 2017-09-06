ec59935f700120927cc43345ceb515c8049c5511
dingwentao <ustc.dingwentao@gmail.com>
2017-09-06 14:18:59 -0500

    update rw.h

--- sz/include/rw.h
+++ sz/include/rw.h
@@ -24,27 +24,18 @@ extern "C" {
 #endif
 
 float** create2DArray_float(size_t m, size_t n);
-float*** create3DArray_float(size_t p, size_t m, size_t n);
 void free2DArray_float(float** data, size_t m);
+float*** create3DArray_float(size_t p, size_t m, size_t n);
 void free3DArray_float(float*** data, size_t p, size_t m);
-
 double** create2DArray_double(size_t m, size_t n);
-double*** create3DArray_double(size_t p, size_t m, size_t n);
 void free2DArray_double(double** data, size_t m);
+double*** create3DArray_double(size_t p, size_t m, size_t n);
 void free3DArray_double(double*** data, size_t p, size_t m);
-
 size_t checkFileSize(char *srcFilePath, int *status);
-unsigned char *readByteData(char *srcFilePath, size_t *byteLength, int *status);
-double *readDoubleData_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-int16_t *readInt16Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-uint16_t *readUInt16Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-int32_t *readInt32Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-uint32_t *readUInt32Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-int64_t *readInt64Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-uint64_t *readUInt64Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-float *readFloatData_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
-double *readDoubleData(char *srcFilePath, size_t *nbEle, int *status);
 
+unsigned char *readByteData(char *srcFilePath, size_t *byteLength, int *status);
+double *readDoubleData(char *srcFilePath, size_t *nbEle, int *status);
+int8_t *readInt8Data(char *srcFilePath, size_t *nbEle, int *status);
 int16_t *readInt16Data(char *srcFilePath, size_t *nbEle, int *status);
 uint16_t *readUInt16Data(char *srcFilePath, size_t *nbEle, int *status);
 int32_t *readInt32Data(char *srcFilePath, size_t *nbEle, int *status);
@@ -52,12 +43,22 @@ uint32_t *readUInt32Data(char *srcFilePath, size_t *nbEle, int *status);
 int64_t *readInt64Data(char *srcFilePath, size_t *nbEle, int *status);
 uint64_t *readUInt64Data(char *srcFilePath, size_t *nbEle, int *status);
 float *readFloatData(char *srcFilePath, size_t *nbEle, int *status);
+unsigned short* readShortData(char *srcFilePath, size_t *dataLength, int *status);
 
-void writeByteData(unsigned char *bytes, size_t outSize, char *tgtFilePath, int *status);
+double *readDoubleData_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+int8_t *readInt8Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+int16_t *readInt16Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+uint16_t *readUInt16Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+int32_t *readInt32Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+uint32_t *readUInt32Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+int64_t *readInt64Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+uint64_t *readUInt64Data_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+float *readFloatData_systemEndian(char *srcFilePath, size_t *nbEle, int *status);
+
+void writeByteData(unsigned char *bytes, size_t byteLength, char *tgtFilePath, int *status);
 void writeDoubleData(double *data, size_t nbEle, char *tgtFilePath, int *status);
 void writeFloatData(float *data, size_t nbEle, char *tgtFilePath, int *status);
 void writeData(void *data, int dataType, size_t nbEle, char *tgtFilePath, int *status);
-
 void writeFloatData_inBytes(float *data, size_t nbEle, char* tgtFilePath, int *status);
 void writeDoubleData_inBytes(double *data, size_t nbEle, char* tgtFilePath, int *status);
 void writeShortData_inBytes(short *states, size_t stateLength, char *tgtFilePath, int *status);
@@ -67,15 +68,13 @@ void writeUIntData_inBytes(unsigned int *states, size_t stateLength, char *tgtFi
 void writeLongData_inBytes(int64_t *states, size_t stateLength, char *tgtFilePath, int *status);
 void writeULongData_inBytes(uint64_t *states, size_t stateLength, char *tgtFilePath, int *status);
 
-unsigned short* readShortData(char *srcFilePath, size_t *dataLength, int *status);
-
-void checkfilesizec_(char *srcFilePath, int *len, size_t *filesize);
-void readbytefile_(char *srcFilePath, int *len, unsigned char *bytes, size_t *byteLength);
-void readdoublefile_(char *srcFilePath, int *len, double *data, size_t *nbEle);
-void readfloatfile_(char *srcFilePath, int *len, float *data, size_t *nbEle);
-void writebytefile_(unsigned char *bytes, size_t *byteLength, char *tgtFilePath, int *len);
-void writedoublefile_(double *data, size_t *nbEle, char *tgtFilePath, int *len);
-void writefloatfile_(float *data, size_t *nbEle, char *tgtFilePath, int *len);
+void checkfilesizec_(char *srcFilePath, size_t *len, int *filesize);
+void readbytefile_(char *srcFilePath, size_t *len, unsigned char *bytes, int *byteLength);
+void readdoublefile_(char *srcFilePath, size_t *len, double *data, int *nbEle);
+void readfloatfile_(char *srcFilePath, size_t *len, float *data, int *nbEle);
+void writebytefile_(unsigned char *bytes, int *byteLength, char *tgtFilePath, size_t *len);
+void writedoublefile_(double *data, size_t *nbEle, char *tgtFilePath, size_t *len);
+void writefloatfile_(float *data, size_t *nbEle, char *tgtFilePath, size_t *len);
 
 #ifdef __cplusplus
 }
