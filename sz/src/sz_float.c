84a563af499c14ba0d785aa558bc162a88ee7e37
Robert Underwood <runderwood@anl.gov>
2022-09-01 13:36:37 -0400

    added pre-encoding size to stats

--- sz/src/sz_float.c
+++ sz/src/sz_float.c
@@ -3020,6 +3020,9 @@ int errBoundMode, double absErr_Bound, double relBoundRatio, double pwRelBoundRa
 		}
 		else if(confparams_cpr->szMode==SZ_BEST_COMPRESSION || confparams_cpr->szMode==SZ_DEFAULT_COMPRESSION || confparams_cpr->szMode==SZ_TEMPORAL_COMPRESSION)
 		{
+#if HAVE_WRITESTATS
+      writePreEncodingSize(tmpOutSize);
+#endif
 			*outSize = sz_lossless_compress(confparams_cpr->losslessCompressor, confparams_cpr->gzipMode, tmpByteData, tmpOutSize, newByteData);
 			free(tmpByteData);
 		}
