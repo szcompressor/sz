8cb1cd1e0f8e763495be2c89e357c2fd6e0d071e
disheng222 <disheng222@gmail.com>
2022-11-09 16:45:50 -0600

    remove useless config.h in many .c files.

--- sz/src/sz_double.c
+++ sz/src/sz_double.c
@@ -7,14 +7,12 @@
  *      See COPYRIGHT in top-level directory.
  */
 
-#include "config.h"
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef HAVE_UNISTD_H
-# include <unistd.h>
-#endif
+#include <unistd.h>
 #include <math.h>
 #include "sz.h"
 #include "CompressElement.h"
--- sz/src/sz_float.c
+++ sz/src/sz_float.c
@@ -8,14 +8,11 @@
  */
 
 
-#include "config.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef HAVE_UNISTD_H
-# include <unistd.h>
-#endif
+#include <unistd.h>
 #include <math.h>
 #include "sz.h"
 #include "CompressElement.h"
